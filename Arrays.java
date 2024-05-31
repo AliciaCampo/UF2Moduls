098e9831ed88cef3eed6689842a9060bd801c02c
AliciaCampo <2023_alicia.campo@iticbcn.cat>
2024-05-31 13:07:00 +0200

    seguiment

--- Arrays.java
+++ Arrays.java
@@ -1,4 +1,13 @@
 public class Arrays {
     
-    
+    public void pujaNota(int [] notes){
+        //este modulo debe mirar si las notas son igual a 5 debe subir la nota 1 punto extra 
+    }
+    public int []  esEnter(String [] numeros ){
+        //este modulo 
+        int [] numerosInt = new int[numeros.length];
+        /*for (int i = 0 ; numeros.length ; i ++){
+        }*/
+        return numerosInt;
+    }
 }
