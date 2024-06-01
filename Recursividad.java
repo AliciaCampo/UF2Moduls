5c196f6a10dc8e1f6c1ff7e1bbd6fbcd0650dde8
AliciaCampo <2023_alicia.campo@iticbcn.cat>
2024-06-01 21:21:40 +0200

    ejemplo de recursvidad completado

--- Recursividad.java
+++ Recursividad.java
@@ -1,9 +1,20 @@
 public class Recursividad {
     public static void main(String[] args){
-        System.out.println("Text?");
+        System.out.println("Hola que modulo quieres usar ? (mostraLletres/sumaDigits)");
+        String resposta = Entrada.readLine();
+        if (resposta.equals("mostraLletres")){
+            System.out.println("Text?");
         String text = Entrada.readLine();
         mostraLletres(text);
         System.out.println();   // un salt de línia final
+        } else if (resposta.equals("sumaDigits")){
+            System.out.println("Text?");
+            String text = Entrada.readLine();
+            int nombres = sumaDigits(text);
+            System.out.println(nombres);
+        }else{
+            System.out.println("Opcion no valida intentalo de nuevo");
+        }
     }
 
     // mostra només les lletres de text
@@ -20,6 +31,20 @@ public class Recursividad {
         // considerem les lletres que conté la resta del text
         String restaText =text.substring(1);  // resta del text
         mostraLletres(restaText);      // crida recursiva
+    }
+    // funcion recursiva que suma los digitos
+     public static int sumaDigits(String text) {
+        // cas base
+        if (text.isEmpty()) {
+            return 0;
+        }
+        // tracta pas actuals
+        char primerChar = text.charAt(0);
+        int primerDigit = Character.isDigit(primerChar) ? Character.digit(primerChar, 10) : 0;
+        // tracta pas recursiu
+        int sumaRestant = sumaDigits(text.substring(1));
+        // composa resultat
+        return primerDigit + sumaRestant;
     }
     
 }
