59022769bcc25f3f4f24f591a3e0f1730fdfb3f2
AliciaCampo <2023_alicia.campo@iticbcn.cat>
2024-06-03 10:41:03 +0200

    nova versio arrays

--- Arrays.java
+++ Arrays.java
@@ -23,6 +23,21 @@ public class Arrays {
             System.out.print(nota + "  |  ");
         }
         System.out.println("");
+        System.out.println("Calculo de numero mas grande de un array : ");
+        numeroMesgran();
+        System.out.println("Numeros invertidos :");
+        invertirArray();
+        System.out.println("Suma de enteros de un array : ");
+        int [] numerosInt = {3,2,1,150};
+        int resultado = sumaEntersArrayInt(numerosInt);
+        System.out.println("El resultado es : " + resultado);
+    }
+    public  static int sumaEntersArrayInt (int [] numeros ){
+        int resultado  = 0 ;
+        for (int i  = 0 ; i < numeros.length ; i ++){
+            resultado += numeros[i];
+        }
+        return resultado;
     }
     
     public static int [] pujaNota(int [] notas){
@@ -64,5 +79,28 @@ public class Arrays {
             }
         }
         return true;
+    }
+    public static void numeroMesgran(){
+        int []  numeros = {1,2,3,4,56,7,8,9,99};
+        int numeroMax = 0;
+        int numeroActual = 0;
+        for (int i = 0; i <numeros.length;i++){
+            if (numeros[i] > numeroMax){
+                numeroMax =  numeros[i];
+            }
+        }
+        System.out.println("El numero mas alto es : " +  numeroMax);
+    }
+    public static void invertirArray(){
+        int [] numeros = {1,2,3,4,5,6,7,8,9};
+        int [] numerosInvertidos = new int[numeros.length];
+        for (int i  = 0; i<numeros.length; i ++ ){
+            numerosInvertidos[i] = numeros[numeros.length -1 -i];
+        }
+        for (int i : numerosInvertidos) {
+            System.out.print(i + " | ");
+        }
+        System.out.println("");
+
     }
 }
