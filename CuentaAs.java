45ecb9809788c2ca9c6a290d8a62a1d5a0165744
Maria Manzano <2023_maria.manzano@iticbcn.cat>
2024-06-04 19:34:05 +0200

    brbrbr slucis finaliza la transmision brbrbr

--- CuentaAs.java
+++ CuentaAs.java
@@ -2,16 +2,17 @@ public class CuentaAs {
     public static void main(String[] args) {
         String [] nombres = {"Alicia","Judith","Maria","Gisela","Keyla","Lucia"};
         int resultado = cuentaAs(nombres);
-        System.out.println("Hay " + resultado + " 'a'");
-        
+        System.out.println("Hay " + resultado + " 'a'");       
     }
     public static int cuentaAs(String [] nombres){
         int contador = 0;
         for (int i = 0; i < nombres.length ; i++){
-            String nombreAct = nombres[i];
-            nombreAct.toLowerCase();
-            if (nombreAct.contains("a")){
-                contador +=1;
+            String nombreAct = nombres[i].toLowerCase();
+            for (int j = 0 ; j < nombreAct.length(); j++){
+                char nombreChar = nombreAct.charAt(j);
+            if (nombreChar == 'a'){
+                contador ++;
+            }
             }
         }
         return contador ;
